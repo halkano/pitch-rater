7b2fe23ec457b9f6cd08a4aea41df7e580856534
halkano <halkanoh10@gmaile.com>
2019-04-26 09:36:11 +0300

    added some importation to my views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,11 @@
-
+from flask import render_template, request, redirect, url_for, abort, flash
+from . import main
+from flask_login import login_required, current_user
+from ..models import Pitch, User, Comment, Upvote, Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm, UpdateProfile
+from flask.views import View, MethodView
+from .. import db, photos
+import markdown2
 
 
 # Views
